f7c72feecf00d26af66cc47c9e7e2128513cedbd
Irteza Syed <101089752+irtezasyed01@users.noreply.github.com>
2023-02-03 09:25:52 +0500

    Create testfile.php

---
+++ testfile.php
@@ -0,0 +1,9 @@
+<?php 
+
+echo "this is a test file created on github repository in Master branch.";
+
+echo "I want to pull this file to my local repository. Hope it works fine.";
+
+echo "Thank you github... You're amazing. ";
+
+?>
